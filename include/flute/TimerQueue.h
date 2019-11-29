ef4f42c65a33478bd734e374fa53fba67c75dd9a
why <1378684171@qq.com>
2019-11-29 11:38:18 +0800

    rename symbol

---
+++ include/flute/TimerQueue.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+ *
+ * File Name:  TimerQueue.h
+ * Repository: https://github.com/TimeLooper/flute
+ * Author:     why
+ * Date:       2019/11/29
+ *
+ *************************************************************************/
+
+#pragma once
+#include <flute/noncopyable.h>
+
+namespace flute {
+
+class TimerQueue : private noncopyable {
+public:
+    TimerQueue();
+    ~TimerQueue();
+
+private:
+    
+};
+
+} // namespace flute
---
+++ src/flute/TimerQueue.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+ *
+ * File Name:  TimerQueue.cpp
+ * Repository: https://github.com/TimeLooper/flute
+ * Author:     why
+ * Date:       2019/11/29
+ *
+ *************************************************************************/
+
+#include <flute/TimerQueue.h>
