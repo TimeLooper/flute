d768dcc29e4e48b4f8b77d7efc3086dc09b372c3
why <1378684171@qq.com>
2019-12-12 00:08:43 +0800

    buffer

--- include/flute/Buffer.h
+++ include/flute/Buffer.h
@@ -9,19 +9,44 @@
 
 #pragma once
 
+#include <string>
 #include <list>
 
 namespace flute {
 
-struct BufferChain;
+struct BufferChain {
+    std::size_t readIndex;
+    std::size_t writeIndex;
+    std::size_t capacity;
+    std::uint8_t* buffer;
+};
 
 class Buffer {
 public:
     Buffer();
     ~Buffer();
 
-    void append
+    std::int8_t peekInt8() const;
+    std::int16_t peekInt16() const;
+    std::int32_t peekInt32() const;
+    std::int64_t peekInt64() const;
+    std::int8_t readInt8();
+    std::int16_t readInt16();
+    std::int32_t readInt32();
+    std::int64_t readInt64();
+    std::string readLine();
+    void appendInt8(std::int8_t value);
+    void appendInt16(std::int16_t value);
+    void appendInt32(std::int32_t value);
+    void appendInt64(std::int64_t value);
+    void appendBufferChain(BufferChain* ch);
+    void append(const void* buffer, std::size_t size);
+
+    static BufferChain* createBufferChain(std::size_t length = 4096);
+    static void destroyBufferChain(BufferChain** chain);
+
 private:
+    void peek(std::uint8_t* buffer, std::size_t length);
     std::list<BufferChain *> m_chains;
 };
 
--- src/flute/Buffer.cpp
+++ src/flute/Buffer.cpp
@@ -11,11 +11,123 @@
 
 namespace flute {
 
-struct BufferChain {
-    std::size_t prependIndex;
-    std::size_t writeIndex;
-    std::size_t capacity;
-    std::uint8_t* buffer;
-};
+Buffer::Buffer() : m_chains() {
+
+}
+
+Buffer::~Buffer() {
+
+}
+
+std::int8_t Buffer::peekInt8() const {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    return *(reinterpret_cast<std::int8_t *>(ch->buffer + ch->readIndex));
+}
+
+std::int16_t Buffer::peekInt16() const {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    return *(reinterpret_cast<std::int16_t *>(ch->buffer + ch->readIndex));
+}
+
+std::int32_t Buffer::peekInt32() const {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    return *(reinterpret_cast<std::int32_t *>(ch->buffer + ch->readIndex));
+}
+
+std::int64_t Buffer::peekInt64() const {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    return *(reinterpret_cast<std::int64_t *>(ch->buffer + ch->readIndex));
+}
+
+std::int8_t Buffer::readInt8() {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    auto result = *(reinterpret_cast<std::int8_t *>(ch->buffer + ch->readIndex));
+    ch->readIndex += sizeof(result);
+    return result;
+}
+
+std::int16_t Buffer::readInt16() {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    auto result = *(reinterpret_cast<std::int16_t *>(ch->buffer + ch->readIndex));
+    ch->readIndex += sizeof(result);
+    return result;
+}
+
+std::int32_t Buffer::readInt32() {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    auto result = *(reinterpret_cast<std::int32_t *>(ch->buffer + ch->readIndex));
+    ch->readIndex += sizeof(result);
+    return result;
+}
+
+std::int64_t Buffer::readInt64() {
+    if (m_chains.empty()) {
+        return 0;
+    }
+    auto ch = m_chains.front();
+    auto result = *(reinterpret_cast<std::int64_t *>(ch->buffer + ch->readIndex));
+    ch->readIndex += sizeof(result);
+    return result;
+}
+
+std::string Buffer::readLine() {
+    if (m_chains.empty()) {
+        return "";
+    }
+    char temp[65536]{};
+    for (const auto ch : m_chains) {
+
+    }
+    return "";
+}
+
+void Buffer::appendInt8(std::int8_t value) {
+
+}
+
+void Buffer::appendInt16(std::int16_t value) {
+
+}
+
+void Buffer::appendInt32(std::int32_t value) {
+
+}
+
+void Buffer::appendInt64(std::int64_t value) {
+
+}
+
+void Buffer::appendBufferChain(BufferChain* ch) {
+
+}
+
+void Buffer::append(const void* buffer, std::size_t size) {
+
+}
+
+void Buffer::peek(std::uint8_t* buffer, std::size_t length) {
+    
+}
 
 } // namespace flute
