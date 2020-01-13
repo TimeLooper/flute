05e4447f378e7afe73906ed3c2ac80a37e58ca93
why <1378684171@qq.com>
2020-01-14 00:06:10 +0800

    fix for FreeBSD

--- include/flute/endian.h
+++ include/flute/endian.h
@@ -10,11 +10,12 @@
 #ifdef FLUTE_HAVE_ENDIAN_H
 #include <endian.h>
 #endif
-#ifdef FLUTE_HAVE_SYS_ENDIAN_H
+#ifdef FLUTE_HAVE_MACHINE_ENDIAN_H
+#ifndef __FreeBSD__
+#include <machine/endian.h>
+#else
 #include <sys/endian.h>
 #endif
-#ifdef FLUTE_HAVE_MACHINE_ENDIAN_H
-#include <machine/endian.h>
 #endif
 #ifdef _WIN32
 #include <WinSock2.h>
@@ -24,7 +25,7 @@
 namespace flute {
 
 inline std::uint64_t host2Network(std::uint64_t value) {
-#if defined(FLUTE_HAVE_ENDIAN_H) || defined(FLUTE_HAVE_SYS_ENDIAN_H)
+#if defined(FLUTE_HAVE_ENDIAN_H) || defined(__FreeBSD__)
     return htobe64(value);
 #endif
 #if (!defined(__FreeBSD__) && defined(FLUTE_HAVE_MACHINE_ENDIAN_H)) || defined(_WIN32)
@@ -35,7 +36,7 @@ inline std::uint64_t host2Network(std::uint64_t value) {
 inline std::int64_t host2Network(std::int64_t value) { return host2Network(static_cast<std::uint64_t>(value)); }
 
 inline std::uint32_t host2Network(std::uint32_t value) {
-#if defined(FLUTE_HAVE_ENDIAN_H) || defined(FLUTE_HAVE_SYS_ENDIAN_H)
+#if defined(FLUTE_HAVE_ENDIAN_H) || defined(__FreeBSD__)
     return htobe32(value);
 #endif
 #if (!defined(__FreeBSD__) && defined(FLUTE_HAVE_MACHINE_ENDIAN_H)) || defined(_WIN32)
@@ -46,7 +47,7 @@ inline std::uint32_t host2Network(std::uint32_t value) {
 inline std::int32_t host2Network(std::int32_t value) { return host2Network(static_cast<std::uint32_t>(value)); }
 
 inline std::uint16_t host2Network(std::uint16_t value) {
-#if defined(FLUTE_HAVE_ENDIAN_H) || defined(FLUTE_HAVE_SYS_ENDIAN_H)
+#if defined(FLUTE_HAVE_ENDIAN_H) || defined(__FreeBSD__)
     return htobe16(value);
 #endif
 #if (!defined(__FreeBSD__) && defined(FLUTE_HAVE_MACHINE_ENDIAN_H)) || defined(_WIN32)
@@ -57,7 +58,7 @@ inline std::uint16_t host2Network(std::uint16_t value) {
 inline std::int16_t host2Network(std::int16_t value) { return host2Network(static_cast<std::uint16_t>(value)); }
 
 inline std::uint64_t network2Host(std::uint64_t value) {
-#if defined(FLUTE_HAVE_ENDIAN_H) || defined(FLUTE_HAVE_SYS_ENDIAN_H)
+#if defined(FLUTE_HAVE_ENDIAN_H) || defined(__FreeBSD__)
     return be64toh(value);
 #endif
 #if (!defined(__FreeBSD__) && defined(FLUTE_HAVE_MACHINE_ENDIAN_H)) || defined(_WIN32)
@@ -68,7 +69,7 @@ inline std::uint64_t network2Host(std::uint64_t value) {
 inline std::int64_t network2Host(std::int64_t value) { return network2Host(static_cast<std::uint64_t>(value)); }
 
 inline std::uint32_t network2Host(std::uint32_t value) {
-#if defined(FLUTE_HAVE_ENDIAN_H) || defined(FLUTE_HAVE_SYS_ENDIAN_H)
+#if defined(FLUTE_HAVE_ENDIAN_H) || defined(__FreeBSD__)
     return be32toh(value);
 #endif
 #if (!defined(__FreeBSD__) && defined(FLUTE_HAVE_MACHINE_ENDIAN_H)) || defined(_WIN32)
@@ -79,7 +80,7 @@ inline std::uint32_t network2Host(std::uint32_t value) {
 inline std::int32_t network2Host(std::int32_t value) { return network2Host(static_cast<std::uint32_t>(value)); }
 
 inline std::uint16_t network2Host(std::uint16_t value) {
-#if defined(FLUTE_HAVE_ENDIAN_H) || defined(FLUTE_HAVE_SYS_ENDIAN_H)
+#if defined(FLUTE_HAVE_ENDIAN_H) || defined(__FreeBSD__)
     return be16toh(value);
 #endif
 #if (!defined(__FreeBSD__) && defined(FLUTE_HAVE_MACHINE_ENDIAN_H)) || defined(_WIN32)
