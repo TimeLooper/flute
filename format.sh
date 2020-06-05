355713a56f5caca68c8e7cf388e0d9d6f92976a2
why <wuhuiyun@hoodinn.com>
2020-06-05 16:56:26 +0800

    tool script

--- format.sh
+++ format.sh
@@ -6,4 +6,3 @@ clang-format -i src/flute/*.cc
 clang-format -i src/flute/*.h
 # clang-format -i src/flute/detail/*.cc
 clang-format -i src/flute/detail/*.h
-clang-format -i examples/gameserver/login/*.cc
