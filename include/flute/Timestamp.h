813ace274d9eba9662da6b86ce492c57b4527491
why <wuhuiyun@hoodinn.com>
2020-06-21 21:35:03 +0800

    Timestamp

--- include/flute/Timestamp.h
+++ include/flute/Timestamp.h
@@ -8,16 +8,45 @@
 #include <flute/config.h>
 #include <flute/copyable.h>
 
+#include <cstdint>
+#include <algorithm>
+
 namespace flute {
 
 class Timestamp : private copyable {
 public:
-    FLUTE_API_DECL Timestamp();
+    FLUTE_API_DECL Timestamp() : m_microSeconds(0) {}
+
+    FLUTE_API_DECL explicit Timestamp(std::int64_t microSeconds) : m_microSeconds(microSeconds) {}
 
     FLUTE_API_DECL ~Timestamp();
 
+    FLUTE_API_DECL bool operator==(const Timestamp& rhs) { return this->m_microSeconds == rhs.m_microSeconds; }
+
+    FLUTE_API_DECL bool operator!=(const Timestamp& rhs) { return !(*this == rhs); }
+
+    FLUTE_API_DECL bool operator<(const Timestamp& rhs) { return this->m_microSeconds < rhs.m_microSeconds; }
+
+    FLUTE_API_DECL bool operator>(const Timestamp& rhs) { return this->m_microSeconds > rhs.m_microSeconds; }
+
+    FLUTE_API_DECL Timestamp& operator=(const Timestamp& rhs) {
+        this->m_microSeconds = rhs.m_microSeconds;
+        return *this;
+    }
+
+    FLUTE_API_DECL Timestamp& operator=(Timestamp&& rhs) {
+        std::swap(this->m_microSeconds, rhs.m_microSeconds);
+        return *this;
+    }
+
+    FLUTE_API_DECL std::int64_t getTime() const { return m_microSeconds; }
+
+    FLUTE_API_DECL void setTime(std::int64_t time) { m_microSeconds = time; }
+
+    static Timestamp now();
+
 private:
-    std::int64_t m_timestamp;
+    std::int64_t m_microSeconds;
 };
 
 } // namespace flute
--- src/flute/Timestamp.cc
+++ src/flute/Timestamp.cc
@@ -4,6 +4,13 @@
 
 #include <flute/Timestamp.h>
 
+#include <flute/internal.h>
+
 namespace flute {
+
+Timestamp Timestamp::now() {
+    return Timestamp(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch())
+        .count());
+}
 
 } // namespace flute
